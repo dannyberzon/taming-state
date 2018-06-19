843bb8d1a2457aeb14b07b4cc43fc2524f60d858
Daniel Berzon <daniel@ocasta.com>
2018-06-19 10:18:11 +0100

    p8

---
+++ src/Counter.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+class CounterContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      counter: 0
+    };
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+  }
+
+  onIncrement() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
+  onDecrement() {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  }
+
+  render() {
+    return <CounterPresenter
+              counter={this.state.counter}
+              onIncrement={this.onIncrement}
+              onDecrement={this.onDecrement}
+            />
+  }
+}
+
+function CounterPresenter(props) { 
+  return (
+    <div>
+      <p>{props.counter}</p>
+      <button type="button" onClick={props.onIncrement}>
+        Increment
+      </button>
+      <button type="button" onClick={props.onDecrement}>
+          Decrement
+      </button>
+    </div>
+  );
+}
+
+
+export default CounterContainer;
