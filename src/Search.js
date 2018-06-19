06b92c6462eee4990cc11f8be8f193825ed9331b
Daniel Berzon <daniel@ocasta.com>
2018-06-19 11:45:02 +0100

    page 13

---
+++ src/Search.js
@@ -0,0 +1,45 @@
+import React from 'react';
+  class Search extends React.Component {
+    constructor(props) {
+      super(props);
+      
+      this.state = {
+        query: ''
+      };
+
+      this.onChange = this.onChange.bind(this);
+      this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(event){
+      const {value} = event.target;
+
+      this.setState({
+        query: value
+      })
+    }
+
+    onSubmit(event) {
+      const { query } = this.state;
+      // do something with the search value
+      // e.g. propagate it up to the parent component this.props.onSearch(value);
+      console.log('query is ', query);
+      event.preventDefault();
+    }
+
+    render() {
+      return (
+        <form onSubmit={this.onSubmit}>
+        <input
+          onChange={this.onChange}
+          type="text"
+        />
+        <button type="submit">
+          Search
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Search
